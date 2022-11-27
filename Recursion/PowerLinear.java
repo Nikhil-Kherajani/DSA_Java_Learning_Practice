50b30206f246bdaab1b70e894d00b7abd13db165
Nikhil-Kherajani <nikhilkherajani7@gmail.com>
2022-11-27 20:12:37 +0530

    recursion basic

---
+++ Recursion/PowerLinear.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class PowerLinear {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println(power(x, n));
+    }
+
+    public static int power(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return x * power(x, n - 1);
+    }
+
+}
