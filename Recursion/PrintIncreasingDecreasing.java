f52a8fb00300a328209967d467fcfa343bb5ff7b
Nikhil-Kherajani <nikhilkherajani7@gmail.com>
2022-11-27 20:09:21 +0530

    recursion basic

---
+++ Recursion/PrintIncreasingDecreasing.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class PrintIncreasingDecreasing {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pdi(n);
+
+    }
+
+    public static void pdi(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+        pdi(n - 1);
+        System.out.println(n);
+    }
+
+}
